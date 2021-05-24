7b147bdda87bbdfd0fe44632e61c9af441c27057
Wilbert Navarrete <wilmion92@gmail.com>
2021-05-24 22:28:57 +0200

    Agregando funcionalidad de correo y mejora en el rd

--- hispanosolutions/js/formulario.js
+++ hispanosolutions/js/formulario.js
@@ -1,56 +1,37 @@
-function enviar() {
- 
-    var messagesElement = document.getElementById("messages");
-    
-    // disable send button
-    document.querySelector("#send").disabled = true;
-    document.querySelector("#send").innerText = "Enviando ...";
-    // show messages
-    document.querySelector("#messages").style.display = "block";
- 
-    // XHR object
-    var xhr = new XMLHttpRequest();
-    
-    xhr.onload = function(res) {        
-        
-        if (xhr.status == 200) {
-            
-            console.log("resultado:"+xhr.status);
-            var res = JSON.parse(this.response);
-            
-            document.querySelector("#messages").innerHTML = res.message;
-            //document.querySelector("#messages").innerHTML += res.msg_phpmailer;
-            console.log(xhr.responseText);          
- 
-            if (res.error == false) {
-                document.querySelector("#contact").style.display = "none";
-                document.querySelector("#messages").classList.remove("alert-danger");
-                document.querySelector("#messages").classList.add("alert-success");
-                document.querySelector("#messages").innerHTML = res.message;
-            } else {
-                document.querySelector("#messages").classList.add("alert-danger");
-                document.querySelector("#send").disabled = false;
-                document.querySelector("#send").innerText = "Reintentar el envío";
-            }            
-            
-        } else {
-            // log error
-            document.querySelector("#send").disabled = false;
-            document.querySelector("#messages").innerHTML = "Se ha producido un error, contacte con el administrador";
-            console.log("error"+xhr.status);
-        }
-    }
-    
-    // preapare request
-    var formMethod = "POST";
-    var formUrl = 'mod/enviar.php';
-    var formType = true;   
- 
-    xhr.open(formMethod, formUrl, formType);
- 
-    // prepare form data
-    var formElement = document.getElementById("contact");
-    var data = new FormData(formElement);
-    xhr.send(data);    
-}
-Recep
+//Obteniendo el form.
+var form = document.getElementById('form_email');
+
+//Detector de Envento (En este caso el de submit)
+form.addEventListener('submit' , (e) => {
+    e.preventDefault() //Elimina su acción por defecto.
+    var button = form.querySelector('button');
+    button.innerText = 'Enviando...';
+    button.disabled = true;
+
+    //Obreniendo Valores
+
+    var subject = document.querySelector('#subject').value //asunto
+    var email = document.querySelector('#email_form').value //email
+    var phone = document.querySelector('#phone').value //telefono
+    var names = document.querySelector('#name').value //nombres
+
+    //Creando el body del email
+
+    var body = `<h1>${subject}</h1>
+    <p>Te llego un nuevo mensaje de tu página web 
+    <br> Email: ${email} <br>
+    Teléfono: ${phone} <br>
+    Nombres y Apellidos: ${names}
+    </p>`
+
+    Email.send({
+        SecureToken: "843ed0a0-b06c-4582-a16e-c8402505f089", //Token de seguridad
+        To: "info@alercasystems.com", //A quién se lo envia.
+        From: email, //De donde viene el correo
+        Subject: subject, //Asunto
+        Body: body //Cuerpo del corréo
+    }).then(msg => {
+        button.innerText = 'Enviar'
+        button.disabled = false;
+    });
+})
